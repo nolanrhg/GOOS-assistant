4cc2e1426ddda42f5cc000a45b03d8caa94986bb
Melchior <nolan.gagnon@ieee.org>
2020-07-08 12:01:19 -0400

    new task is to find net income datapoints with regex

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## Task One
 Get links to annual reports on EDGAR
+
+## Task Two
+Get net income data point from each annual report
--- edgar_harvester.py
+++ edgar_harvester.py
@@ -3,62 +3,70 @@ import requests # for downloading webpages
 import re # for regular expressions
 import sys # for reading command line args
 
-###########################
-## Ticker to CIK dictionary
-###########################
+#--------------------------
+# Ticker to CIK dictionary
+#--------------------------
 t2c_dict = {"GOOS" : "1690511", "UPS" : "1090727", "SNAP" : "1564408"}
 
+'''
+|||||||||||||||
+|| FUNCTIONS ||
+|||||||||||||||
+'''
+
 #################################################
+##                                             ##
 ## Purpose: Find all links to annual reports   ##
 ##          on EDGAR for company corresponding ##
 ##          to ticker. If company is domestic, ##
 ##          the annual report is called a      ##
 ##          10-K; otherwise it is called a     ##
 ##          20-F.                              ##
+##                                             ##
 #################################################
 def get_ar_links(ticker, domestic = True):
 
-    ##################################### 
+    #------------------------------------
     # Storage for links to annual reports
-    #####################################
+    #------------------------------------
     ar_links = []
     
-    #####################
+    #--------------------
     # Determine form type
-    #####################
+    #--------------------
     form_type = "10-K" if domestic else "20-F" 
     
-    #######################
+    #----------------------
     # Convert ticker to cik
-    #######################
+    #----------------------
     cik = t2c_dict[ticker]
     
-    ################################################
+    #-----------------------------------------------
     # SEC page where annual report links are located
-    ################################################
+    #-----------------------------------------------
     edgar_entry_url = "https://www.sec.gov/cgi-bin/browse-edgar?CIK=" + cik + \
                       "&type=" + form_type + "&dateb=&owner=include&count=100"
 
-    ##################################################
+    #-------------------------------------------------
     # Use BeautifulSoup to extract annual report links
     # from HTML of SEC page
-    ##################################################
+    #-------------------------------------------------
     webpage = requests.get(edgar_entry_url).content
     soup = BeautifulSoup(webpage, 'lxml')
     links = soup.find_all('a', href = re.compile(r"Archives\S*"))
     
-    #####################
-    # store all the links
-    #####################
+    #--------------------
+    # Store all the links
+    #--------------------
     for link in links:
         ar_links.append("https://www.sec.gov" + link.get('href'))
 
     print(ar_links)
     print(len(ar_links))
 
-##############################
+#-----------------------------
 # Get ticker from command line
-##############################
+#-----------------------------
 if (len(sys.argv) < 3):
     print("INSUFFICIENT ARGUMENTS\nUsage: python3 edgar_harvester.py <TICKER> foreign/domestic")
     exit()
@@ -67,7 +75,7 @@ elif (sys.argv[2] != "foreign" and sys.argv[2] != "domestic"):
     exit()
 
 tckr = sys.argv[1]
-###################
+#------------------
 # Test the function 
-###################
+#------------------
 get_ar_links(tckr, domestic = (sys.argv[2] == "domestic"))
