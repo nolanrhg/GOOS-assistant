95bc865e7da81b492169f1efd7aac31f0988420d
Melchior <nolan.gagnon@ieee.org>
2020-07-08 11:05:58 -0400

    using tickers instead of ciks

--- edgar_harvester.py
+++ edgar_harvester.py
@@ -2,23 +2,57 @@ from bs4 import BeautifulSoup
 import requests
 import re # regular expressions library
 
+###########################
+## Ticker to CIK dictionary
+###########################
+t2c_dict = {"GOOS" : "1690511", "UPS" : "1090727", "SNAP" : "1564408"}
+
 #################################################
 ## Purpose: Find all links to annual reports   ##
 ##          on EDGAR for company corresponding ##
-##          to ticker.                         ##
+##          to ticker. If company is domestic, ##
+##          the annual report is called a      ##
+##          10-K; otherwise it is called a     ##
+##          20-F.                              ##
 #################################################
-def get_ap_links(cik):
-    
-    f20f_links = []
+def get_ap_links(ticker, domestic = True):
 
-    edgar_entry_url = "https://www.sec.gov/cgi-bin/browse-edgar?CIK=" + cik + "&type=20-F&dateb=&owner=include&count=100"
+    ##################################### 
+    # Storage for links to annual reports
+    #####################################
+    ap_links = []
+    
+    #####################
+    # Determine form type
+    #####################
+    form_type = "10-K" if domestic else "20-F" 
+    
+    #######################
+    # Convert ticker to cik
+    #######################
+    cik = t2c_dict[ticker]
+    
+    ################################################
+    # SEC page where annual report links are located
+    ################################################
+    edgar_entry_url = "https://www.sec.gov/cgi-bin/browse-edgar?CIK=" + cik + \
+                      "&type=" + form_type + "&dateb=&owner=include&count=100"
+
+    ##################################################
+    # Use BeautifulSoup to extract annual report links
+    # from HTML of SEC page
+    ##################################################
     webpage = requests.get(edgar_entry_url).content
     soup = BeautifulSoup(webpage, 'lxml')
     links = soup.find_all('a', href = re.compile(r"Archives\S*"))
-
+    
+    #####################
+    # store all the links
+    #####################
     for link in links:
-        f20f_links.append("https://www.sec.gov" + link.get('href'))
+        ap_links.append("https://www.sec.gov" + link.get('href'))
 
-    print(f20f_links)
+    print(ap_links)
+    print(len(ap_links))
 
-get_ap_links("1690511")
+get_ap_links("GOOS", domestic = False)
