84751306228575a2488a6da40cd28d5f4002b206
Melchior <nolan.gagnon@ieee.org>
2020-07-09 15:42:36 -0400

    identifying regex's that can be used to find annual reports for various companies

---
+++ regex_lookup_values.py
@@ -0,0 +1,7 @@
+#########################################################
+##                                                     ##
+## Purpose: List of regular expressions used to find   ##
+##          annual report links in EDGAR.              ##
+##                                                     ##
+#########################################################
+GOOS_20F = "cg-20\S*"
